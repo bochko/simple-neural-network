2e57002cb6d72d0bc7864c4ea30a389875ce01b5
Boyan Atanasov <b.atanasov@icloud.com>
2020-01-11 15:08:09 +0000

    indent changes in snn_config

--- snn_config.h
+++ snn_config.h
@@ -9,10 +9,10 @@
 
 typedef double                  floating_type;
 
-typedef std::vector<int> topology_vector;
+typedef std::vector<int>        topology_vector;
 
 
-#define INPUT_LAYER_IDX                 0 // always 0
-#define FUNKY_DECORATOR                 "***"
+#define INPUT_LAYER_IDX         0 // always 0
+#define FUNKY_DECORATOR         "***"
 
 #endif //SIMPLENEURALNETWORK_CONFIG_H
